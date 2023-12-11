3d9157a3c21ed5bf89221fb6193158feecb93eea
leeyongchun <dldydcns123@gmail.com>
2023-12-11 21:48:07 +0900

    테스트 디렉토리 생성

---
+++ module-k8s/src/test/java/com/xiilab/modulek8s/ModuleK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiilab.modulek8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
