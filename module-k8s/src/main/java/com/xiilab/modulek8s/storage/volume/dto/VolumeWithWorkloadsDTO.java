af892f2d2b8990cd7688678e9e8a0925588be21c
leeyongchun <dldydcns123@gmail.com>
2023-12-14 16:17:09 +0900

    uyuni-backend-53 사용자 - 볼륨 상세조회 기능
    1. 해당 볼륨을 사용중인 워크로드를 포함한 볼륨 정보 상세 조회 기능

--- module-k8s/src/main/java/com/xiilab/modulek8s/storage/volume/dto/VolumeWithWorkloadsDTO.java
+++ module-k8s/src/main/java/com/xiilab/modulek8s/storage/volume/dto/VolumeWithWorkloadsDTO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.xiilab.modulek8s.common.enumeration.ResourceType;
 import com.xiilab.modulek8s.common.vo.K8SResourceResVO;
+import com.xiilab.modulek8s.storage.storageclass.enums.StorageType;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import lombok.Builder;
@@ -17,12 +18,14 @@ public class VolumeWithWorkloadsDTO extends K8SResourceResVO {
 	List<String> workloadNames;
 	//용량
 	private String requestVolume;
+	private StorageType storageType;
 
 	@Builder
-	public VolumeWithWorkloadsDTO(HasMetadata hasMetadata, List<String> workloadNames, String requestVolume) {
+	public VolumeWithWorkloadsDTO(HasMetadata hasMetadata, List<String> workloadNames, String requestVolume, StorageType storageType) {
 		super(hasMetadata);
 		this.requestVolume = requestVolume;
 		this.workloadNames = workloadNames;
+		this.storageType = storageType;
 	}
 
 	@Override
