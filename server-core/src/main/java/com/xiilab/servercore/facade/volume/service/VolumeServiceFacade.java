480dc582e63bf6e4081f3c214744bf3d431a4178
leeyongchun <dldydcns123@gmail.com>
2023-12-15 16:38:53 +0900

    uyuni-backend-51 사용자 - 볼륨 삭제 기능
    style: DTO 이름 변경 및 패키지 변경

--- server-core/src/main/java/com/xiilab/servercore/facade/volume/service/VolumeServiceFacade.java
+++ server-core/src/main/java/com/xiilab/servercore/facade/volume/service/VolumeServiceFacade.java
@@ -1,7 +1,7 @@
 package com.xiilab.servercore.facade.volume.service;
 
-import com.xiilab.servercore.volume.dto.CreateReqDTO;
+import com.xiilab.servercore.volume.dto.CreateVolumeReqDTO;
 
 public interface VolumeServiceFacade {
-	void createVolume(CreateReqDTO requestDTO);
+	void createVolume(CreateVolumeReqDTO requestDTO);
 }
