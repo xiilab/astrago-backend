830a027a40ea68357e9152d4ef4fae0c343d25a3
hc.park <hc.park@xiilab.com>
2023-12-20 21:45:35 +0900

    workspace repository 생성

---
+++ module-k8s/src/main/java/com/xiilab/modulek8s/workspace/repository/WorkspaceRepoImpl.java
@@ -0,0 +1,53 @@
+package com.xiilab.modulek8s.workspace.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.xiilab.modulek8s.config.K8sAdapter;
+import com.xiilab.modulek8s.workspace.vo.WorkspaceReqVO;
+import com.xiilab.modulek8s.workspace.vo.WorkspaceResVO;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class WorkspaceRepoImpl implements WorkspaceRepo {
+	private final K8sAdapter k8sAdapter;
+
+	@Override
+	public WorkspaceResVO createWorkSpace(WorkspaceReqVO workspaceReqVO) {
+		Namespace namespace = (Namespace)createResource(workspaceReqVO.createResource());
+		return new WorkspaceResVO(namespace);
+	}
+
+	@Override
+	public WorkspaceResVO getWorkspaceByName(String name) {
+		KubernetesClient kubernetesClient = k8sAdapter.configServer();
+		Namespace namespace = kubernetesClient.namespaces().withName(name).get();
+		return new WorkspaceResVO(namespace);
+	}
+
+	@Override
+	public List<WorkspaceResVO> getWorkspaceList() {
+		KubernetesClient kubernetesClient = k8sAdapter.configServer();
+		List<Namespace> items = kubernetesClient.namespaces().list().getItems();
+		return items.stream().map(WorkspaceResVO::new).toList();
+	}
+
+	@Override
+	public void deleteWorkspaceByName(String name) {
+		try (KubernetesClient kubernetesClient = k8sAdapter.configServer()) {
+			kubernetesClient.namespaces().withName(name).delete();
+		}
+	}
+
+	private HasMetadata createResource(HasMetadata hasMetadata) {
+		try (KubernetesClient kubernetesClient = k8sAdapter.configServer()) {
+			return kubernetesClient.resource(hasMetadata).create();
+		}
+	}
+}
