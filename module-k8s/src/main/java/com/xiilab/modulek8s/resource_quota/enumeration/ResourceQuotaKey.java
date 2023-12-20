4c84df1136d900e34eca90071d2d2ecb9fc8beb8
hc.park <hc.park@xiilab.com>
2023-12-20 21:40:43 +0900

    ResourceQuota dto enum vo 생성

---
+++ module-k8s/src/main/java/com/xiilab/modulek8s/resource_quota/enumeration/ResourceQuotaKey.java
@@ -0,0 +1,19 @@
+package com.xiilab.modulek8s.resource_quota.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResourceQuotaKey {
+	REQUEST_CPU_KEY("requests.cpu"),
+	REQUEST_MEMORY_KEY("requests.memory"),
+	REQUEST_GPU_KEY("requests.nvidia.com/gpu"),
+	REQUEST_DISK_KEY("requests.storage"),
+	LIMITS_CPU_KEY("limits.cpu"),
+	LIMITS_MEMORY_KEY("limits.memory"),
+	LIMITS_GPU_KEY("limits.nvidia.com/gpu"),
+	LIMITS_DISK_KEY("limits.storage");
+
+	private final String key;
+}
