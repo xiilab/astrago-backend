e7394a54e6ff04157ee2ee98e4e0b96aded9c3f2
DongJu <dj.chung@xiilab.com>
2023-12-20 18:45:03 +0900

    [FIX] API 설명 추가

--- server-core/src/main/java/com/xiilab/servercore/user/controller/GroupController.java
+++ server-core/src/main/java/com/xiilab/servercore/user/controller/GroupController.java
@@ -6,6 +6,7 @@ import com.xiilab.moduleuser.dto.GroupSummaryDTO;
 import com.xiilab.moduleuser.dto.GroupUserDTO;
 import com.xiilab.servercore.common.dto.SearchCondition;
 import com.xiilab.servercore.user.service.GroupFacadeService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,22 +21,26 @@ public class GroupController {
 	private final GroupFacadeService groupFacadeService;
 
 	@GetMapping()
+	@Operation(summary = "그룹 리스트 조회")
 	public ResponseEntity<List<GroupSummaryDTO>> getGroupList(@ModelAttribute SearchCondition searchCondition) {
 		return ResponseEntity.ok(groupFacadeService.getGroupList(searchCondition));
 	}
 
 	@GetMapping("/{id}")
+	@Operation(summary = "그룹 상세 정보 조회")
 	public ResponseEntity<GroupInfoDTO> getGroupInfoById(@PathVariable(name = "id") String id) {
 		return ResponseEntity.ok(groupFacadeService.getGroupInfoById(id));
 	}
 
 	@PostMapping()
+	@Operation(summary = "그룹 생성")
 	public ResponseEntity<HttpStatus> createAccountGroup(@RequestBody GroupReqDTO groupReqDTO) {
 		groupFacadeService.createAccountGroup(groupReqDTO);
 		return ResponseEntity.ok().build();
 	}
 
 	@PostMapping("/{groupId}/members")
+	@Operation(summary = "그룹 멤버 추가")
 	public ResponseEntity<HttpStatus> addGroupMember(@PathVariable(name = "groupId") String groupId,
 													 @RequestBody List<String> userIdList) {
 		groupFacadeService.addGroupMember(groupId, userIdList);
@@ -43,12 +48,14 @@ public class GroupController {
 	}
 
 	@DeleteMapping("/{id}")
+	@Operation(summary = "그룹 삭제")
 	public ResponseEntity<HttpStatus> deleteGroupById(@PathVariable(name = "id") String id) {
 		groupFacadeService.deleteGroupById(id);
 		return ResponseEntity.ok().build();
 	}
 
 	@GetMapping("/{groupId}/users")
+	@Operation(summary = "그룹 멤버 조회")
 	public ResponseEntity<List<GroupUserDTO>> getGroupUsers(@PathVariable(name = "groupId") String groupId) {
 		return ResponseEntity.ok(groupFacadeService.getGroupUsers(groupId));
 	}
