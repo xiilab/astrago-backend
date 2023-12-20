26af98fe200417167d46c83bb93ef72595470e41
hc.park <hc.park@xiilab.com>
2023-12-20 21:39:00 +0900

    공통모듈 수정

--- module-k8s/src/main/java/com/xiilab/modulek8s/common/vo/K8SResourceResVO.java
+++ module-k8s/src/main/java/com/xiilab/modulek8s/common/vo/K8SResourceResVO.java
@@ -1,6 +1,8 @@
 package com.xiilab.modulek8s.common.vo;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 import com.xiilab.modulek8s.common.enumeration.AnnotationField;
@@ -9,12 +11,14 @@ import com.xiilab.modulek8s.common.enumeration.ResourceType;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 /**
  * K8SResourceReq 클래스는 Kubernetes 리소스 요청 생성을 위한 기반 역할을 하는 추상 클래스입니다.
  * 다양한 Kubernetes 리소스 신청시 일반적으로 사용되는 속성과 메서드가 포함되어 있습니다.
  */
 @Getter
+@SuperBuilder
 public abstract class K8SResourceResVO {
 	String uid;
 	String resourceName;
@@ -46,6 +50,7 @@ public abstract class K8SResourceResVO {
 			this.resourceName = hasMetadata.getMetadata().getName();
 			this.name = hasMetadata.getMetadata().getName();
 			this.description = null;
+			this.createdAt = convertK8sUtcTimeString(hasMetadata.getMetadata().getCreationTimestamp());
 		}
 
 	}
@@ -55,5 +60,12 @@ public abstract class K8SResourceResVO {
 
 	private boolean isControlledByAstra(Map<String, String> map) {
 		return map != null && "astra".equals(map.get("control-by"));
+	}
+
+	private LocalDateTime convertK8sUtcTimeString(String utcTimeStr) {
+		// UTC 문자열을 ZonedDateTime 객체로 변환
+		ZonedDateTime utcTime = ZonedDateTime.parse(utcTimeStr, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssX"));
+		// ZonedDateTime 객체를 LocalDateTime으로 변환
+		return utcTime.toLocalDateTime();
 	}
 }
