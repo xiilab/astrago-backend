6d355b4be51a6511835f2ec45610324863a8046b
DongJu <dj.chung@xiilab.com>
2023-12-21 21:18:04 +0900

    security filter 수정

--- server-core/src/main/java/com/xiilab/servercore/security/WebSecurityConfig.java
+++ server-core/src/main/java/com/xiilab/servercore/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.xiilab.servercore.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,8 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
-import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
@@ -23,27 +22,17 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.GET,"/docs/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/faqs").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/faqs/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/notices").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/tos").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/tos/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/notices/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/categories").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/app").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/preview/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/test").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/manager/user/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/manager/app/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/user").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/user/join").permitAll()
                 .requestMatchers(HttpMethod.GET,"/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated()
-                );
+                .anyRequest().authenticated());
 
         http.oauth2ResourceServer((oauth2) -> oauth2
             .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter)));
         http.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.disable());
+        http.csrf().disable();
         return http.build();
     }
 }
