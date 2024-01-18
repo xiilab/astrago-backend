c8d00410ab025a8ec7e46f5bdb2b0ffdc7d3c799
hc.park <hc.park@xiilab.com>
2024-01-18 14:44:53 +0900

    :hammer: container의 자원을 리턴하기 위한 dto

---
+++ module-k8s/src/main/java/com/xiilab/modulek8s/common/dto/ResourceDTO.java
@@ -0,0 +1,13 @@
+package com.xiilab.modulek8s.common.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ResourceDTO {
+	private Integer cpuReq;
+	private Integer memReq;
+	private Integer gpuReq;
+	private Integer diskReq;
+}
