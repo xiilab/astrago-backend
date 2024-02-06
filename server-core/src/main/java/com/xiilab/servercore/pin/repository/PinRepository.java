bf22d4f41b02b1341191fb86505e8e5bcf796ba0
hc.park <hc.park@xiilab.com>
2024-02-06 12:00:05 +0900

    :bug: pin bug fix

--- server-core/src/main/java/com/xiilab/servercore/pin/repository/PinRepository.java
+++ server-core/src/main/java/com/xiilab/servercore/pin/repository/PinRepository.java
@@ -3,6 +3,7 @@ package com.xiilab.servercore.pin.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.xiilab.servercore.pin.entity.PinEntity;
@@ -10,9 +11,11 @@ import com.xiilab.servercore.pin.enumeration.PinType;
 
 @Repository
 public interface PinRepository extends JpaRepository<PinEntity, Long> {
-	List<PinEntity> findByTypeAndAndRegUser_RegUserId(PinType type, String id);
+	PinEntity findByTypeAndResourceNameAndRegUser_RegUserId(PinType type, String resourceId, String id);
 
-	long deleteByTypeAndResourceIdAndRegUser_RegUserId(PinType type, String resourceId, String id);
+	@Query("select t from TB_PIN t where t.type = ?1 and t.regUser.regUserId = ?2")
+	List<PinEntity> findByTypeAndRegUser_RegUserId(PinType type, String regUserId);
 
-	PinEntity findByTypeAndResourceIdAndRegUser_RegUserId(PinType type, String resourceId, String id);
+	void deleteByTypeAndResourceNameAndRegUser_RegUserId(PinType type, String resourceName, String regUserId);
+
 }
