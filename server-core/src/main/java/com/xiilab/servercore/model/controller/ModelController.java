745a9018bc758679da5e6565f50dcc51fced0ca2
leeyongchun <dldydcns123@gmail.com>
2024-02-13 12:20:35 +0900

    fix : swagger response type 명시

--- server-core/src/main/java/com/xiilab/servercore/model/controller/ModelController.java
+++ server-core/src/main/java/com/xiilab/servercore/model/controller/ModelController.java
@@ -2,6 +2,7 @@ package com.xiilab.servercore.model.controller;
 
 import java.util.List;
 
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +27,11 @@ import com.xiilab.servercore.model.service.ModelFacadeService;
 import com.xiilab.servercore.model.service.ModelService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -39,7 +45,7 @@ public class ModelController {
 
 	@PostMapping("/models/astrago")
 	@Operation(summary = "아스트라고 model 생성")
-	public ResponseEntity insertAstragoModel(
+	public ResponseEntity<HttpStatus> insertAstragoModel(
 		@RequestPart(name = "createModel") ModelDTO.CreateAstragoModel createModelDTO,
 		@RequestPart(name = "files", required = false) List<MultipartFile> files) {
 		modelFacadeService.insertAstragoDataset(createModelDTO, files);
@@ -48,16 +54,16 @@ public class ModelController {
 
 	@PostMapping("/models/local")
 	@Operation(summary = "로컬 model 생성")
-	public ResponseEntity insertLocalModel(@RequestBody ModelDTO.CreateLocalModel createLocalModel) {
+	public ResponseEntity<HttpStatus> insertLocalModel(@RequestBody ModelDTO.CreateLocalModel createLocalModel) {
 		modelFacadeService.insertLocalModel(createLocalModel);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 
 	@GetMapping("/models")
 	@Operation(summary = "model 전체 조회")
-	public ResponseEntity getModels(@RequestParam(required = false, defaultValue = "1", value = "page") int pageNo,
+	public ResponseEntity<ModelDTO.ResModels> getModels(@RequestParam(required = false, defaultValue = "1", value = "page") int pageNo,
 		@RequestParam(required = false, defaultValue = "10", value = "pageSize") int pageSize,
-		UserInfoDTO userInfoDTO){
+		@Parameter(hidden = true) UserInfoDTO userInfoDTO){
 		ModelDTO.ResModels models = modelService.getModels(pageNo, pageSize, userInfoDTO);
 
 		return new ResponseEntity(models, HttpStatus.OK);
@@ -65,30 +71,31 @@ public class ModelController {
 
 	@GetMapping("/models/{modelId}")
 	@Operation(summary = "model 단건 조회")
-	public ResponseEntity getModel(@PathVariable(name = "modelId") Long modelId){
+	public ResponseEntity<ModelDTO.ResModelWithStorage> getModel(@PathVariable(name = "modelId") Long modelId){
 		ModelDTO.ResModelWithStorage model = modelFacadeService.getModel(modelId);
 		return new ResponseEntity(model, HttpStatus.OK);
 	}
 
 	@PutMapping("/models/{modelId}")
 	@Operation(summary = "model 수정")
-	public ResponseEntity modifyModel(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<HttpStatus> modifyModel(@PathVariable(name = "modelId") Long modelId,
 		@RequestBody ModelDTO.ModifyModel modifyModel,
-		UserInfoDTO userInfoDTO){
+		@Parameter(hidden = true) UserInfoDTO userInfoDTO){
 		modelFacadeService.modifyModel(modifyModel, modelId, userInfoDTO);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 
 	@DeleteMapping("/models/{modelId}")
 	@Operation(summary = "model 삭제")
-	public ResponseEntity deleteModel(@PathVariable(name = "modelId") Long modelId, UserInfoDTO userInfoDTO){
+	public ResponseEntity<HttpStatus> deleteModel(@PathVariable(name = "modelId") Long modelId,
+		@Parameter(hidden = true) UserInfoDTO userInfoDTO){
 		modelFacadeService.deleteModel(modelId, userInfoDTO);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 
 	@GetMapping("/models/astrago/{modelId}/files")
 	@Operation(summary = "astrago model 파일리스트 조회")
-	public ResponseEntity getAstragoModelFiles(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<DirectoryDTO> getAstragoModelFiles(@PathVariable(name = "modelId") Long modelId,
 		@RequestParam(value = "filePath") String filePath){
 		DirectoryDTO files = modelService.getAstragoModelFiles(modelId, filePath);
 		return new ResponseEntity(files, HttpStatus.OK);
@@ -96,7 +103,7 @@ public class ModelController {
 
 	@GetMapping("/models/astrago/{modelId}/file")
 	@Operation(summary = "astrago model 파일 상세 조회")
-	public ResponseEntity getAstragoModelFileInfo(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<ModelDTO.FileInfo> getAstragoModelFileInfo(@PathVariable(name = "modelId") Long modelId,
 		@RequestParam(value = "filePath") String filePath){
 		ModelDTO.FileInfo fileInfo = modelFacadeService.getAstragoModelFileInfo(modelId, filePath);
 		return new ResponseEntity(fileInfo, HttpStatus.OK);
@@ -104,7 +111,7 @@ public class ModelController {
 
 	@GetMapping("/models/astrago/{modelId}/preview")
 	@Operation(summary = "astrago model 파일 미리 보기")
-	public ResponseEntity getAstragoModelFile(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<Resource> getAstragoModelFile(@PathVariable(name = "modelId") Long modelId,
 		@RequestParam(value = "filePath") String filePath){
 		DownloadFileResDTO file = modelFacadeService.getAstragoModelFile(modelId, filePath);
 		HttpHeaders headers = new HttpHeaders();
@@ -114,7 +121,7 @@ public class ModelController {
 
 	@PostMapping("/models/astrago/{modelId}/files/upload")
 	@Operation(summary = "astrago model 파일 업로드")
-	public ResponseEntity astragoModelUploadFile(
+	public ResponseEntity<HttpStatus> astragoModelUploadFile(
 		@PathVariable(name = "modelId") Long modelId,
 		@RequestPart(name = "path") String path,
 		@RequestPart(name = "files") List<MultipartFile> files){
@@ -123,14 +130,14 @@ public class ModelController {
 	}
 	@PostMapping("/models/astrago/{modelId}/directory")
 	@Operation(summary = "astrago model 폴더 생성")
-	public ResponseEntity astragoModelCreateDirectory(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<HttpStatus> astragoModelCreateDirectory(@PathVariable(name = "modelId") Long modelId,
 		@RequestBody ModelDTO.ReqFilePathDTO reqFilePathDTO){
 		modelService.astragoModelCreateDirectory(modelId, reqFilePathDTO);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 	@PostMapping("/models/astrago/{modelId}/files/delete")
 	@Operation(summary = "astrago model 파일, 디렉토리 삭제")
-	public ResponseEntity astragoModelDeleteFiles(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<HttpStatus> astragoModelDeleteFiles(@PathVariable(name = "modelId") Long modelId,
 		@RequestBody ModelDTO.ReqFilePathDTO reqFilePathDTO){
 		modelService.astragoModelDeleteFiles(modelId, reqFilePathDTO);
 		return new ResponseEntity(HttpStatus.OK);
@@ -138,7 +145,7 @@ public class ModelController {
 
 	@GetMapping("/models/astrago/{modelId}/files/download")
 	@Operation(summary = "astrago model 파일, 디렉토리 다운로드")
-	public ResponseEntity downloadAstragoModelFile(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<Resource> downloadAstragoModelFile(@PathVariable(name = "modelId") Long modelId,
 		@RequestParam(value = "filePath") String filePath){
 		DownloadFileResDTO downloadFileResDTO = modelService.DownloadAstragoModelFile(modelId, filePath);
 
@@ -149,7 +156,7 @@ public class ModelController {
 
 	@GetMapping("/models/local/{modelId}/files")
 	@Operation(summary = "local model 파일, 디렉토리 리스트 조회")
-	public ResponseEntity getLocalModelFiles(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<DirectoryDTO> getLocalModelFiles(@PathVariable(name = "modelId") Long modelId,
 		@RequestParam(value = "filePath") String filePath){
 		DirectoryDTO files = modelFacadeService.getLocalModelFiles(modelId,
 			filePath);
@@ -158,7 +165,7 @@ public class ModelController {
 
 	@GetMapping("/models/local/{modelId}/files/download")
 	@Operation(summary = "local model 파일 다운로드")
-	public ResponseEntity DownloadLocalModelFile(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<Resource> DownloadLocalModelFile(@PathVariable(name = "modelId") Long modelId,
 		@RequestParam(value = "filePath") String filePath){
 		DownloadFileResDTO file = modelFacadeService.DownloadLocalModelFile(modelId,
 			filePath);
@@ -169,7 +176,7 @@ public class ModelController {
 
 	@GetMapping("/models/local/{modelId}/file")
 	@Operation(summary = "local model 파일 상세 조회")
-	public ResponseEntity getLocalModelFileInfo(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<ModelDTO.FileInfo> getLocalModelFileInfo(@PathVariable(name = "modelId") Long modelId,
 		@RequestParam(value = "filePath") String filePath){
 		ModelDTO.FileInfo fileInfo = modelFacadeService.getLocalModelFileInfo(modelId,
 			filePath);
@@ -177,7 +184,7 @@ public class ModelController {
 	}
 	@GetMapping("/models/local/{modelId}/preview")
 	@Operation(summary = "local model 파일 미리 보기")
-	public ResponseEntity getLocalModelFile(@PathVariable(name = "modelId") Long modelId,
+	public ResponseEntity<Resource> getLocalModelFile(@PathVariable(name = "modelId") Long modelId,
 		@RequestParam(value = "filePath") String filePath){
 		DownloadFileResDTO file = modelFacadeService.getLocalModelFile(modelId, filePath);
 		HttpHeaders headers = new HttpHeaders();
