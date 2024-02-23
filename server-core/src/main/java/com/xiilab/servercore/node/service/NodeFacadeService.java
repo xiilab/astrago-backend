31e8731a59bee88287bd94c19bbef451ece5fa3e
leeyongchun <dldydcns123@gmail.com>
2024-02-23 16:06:00 +0900

    feat : node 전체 조회 api 구현

--- server-core/src/main/java/com/xiilab/servercore/node/service/NodeFacadeService.java
+++ server-core/src/main/java/com/xiilab/servercore/node/service/NodeFacadeService.java
@@ -1,11 +1,17 @@
 package com.xiilab.servercore.node.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.xiilab.modulecommon.util.DataConverterUtil;
 import com.xiilab.modulek8s.node.dto.ResponseDTO;
 import com.xiilab.modulek8s.node.repository.NodeRepository;
+import com.xiilab.modulemonitor.dto.RequestDTO;
+import com.xiilab.modulemonitor.enumeration.Promql;
+import com.xiilab.modulemonitor.service.PrometheusService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,10 +19,77 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class NodeFacadeService {
 	private final NodeRepository nodeRepository;
+	private final PrometheusService prometheusService;
 
-	public List<ResponseDTO.NodeDTO> getNodeList(){
+	public List<ResponseDTO.NodeDTO> getNodeList() {
 		List<ResponseDTO.NodeDTO> nodeList = nodeRepository.getNodeList();
-		return null;
+		RequestDTO requestDTO = new RequestDTO();
+
+		Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> totalCpuMap = getMetricMap(requestDTO,
+			Promql.TOTAL_NODE_CPU_CORE);
+		Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> usageCpuMap = getMetricMap(requestDTO,
+			Promql.USAGE_NODE_CPU_CORE);
+		Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> totalGpuMap = getMetricMap(requestDTO,
+			Promql.TOTAL_NODE_GPU_COUNT);
+		Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> usageGpuMap = getMetricMap(requestDTO,
+			Promql.USAGE_NODE_GPU_COUNT);
+		Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> totalMemMap = getMetricMap(requestDTO,
+			Promql.TOTAL_NODE_MEMORY_SIZE);
+		Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> usageMemMap = getMetricMap(requestDTO,
+			Promql.USAGE_NODE_MEMORY_SIZE);
+		Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> totalDiskMap = getMetricMap(requestDTO,
+			Promql.NODE_ROOT_DISK_SIZE);
+		Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> usageDiskMap = getMetricMap(requestDTO,
+			Promql.NODE_ROOT_DISK_USAGE_SIZE);
+
+		for (ResponseDTO.NodeDTO nodeDTO : nodeList) {
+			String nodeName = nodeDTO.getNodeName();
+			nodeDTO.setTotalCPU(getValueFromMap(totalCpuMap, nodeName, Promql.TOTAL_NODE_CPU_CORE));
+			nodeDTO.setRequestCPU(getValueFromMap(usageCpuMap, nodeName, Promql.USAGE_NODE_CPU_CORE));
+			nodeDTO.setTotalGPU(getValueFromMap(totalGpuMap, nodeName, Promql.TOTAL_NODE_GPU_COUNT));
+			nodeDTO.setRequestGPU(getValueFromMap(usageGpuMap, nodeName, Promql.USAGE_NODE_GPU_COUNT));
+			nodeDTO.setTotalMEM(getValueFromMap(totalMemMap, nodeName, Promql.TOTAL_NODE_MEMORY_SIZE));
+			nodeDTO.setRequestMEM(getValueFromMap(usageMemMap, nodeName, Promql.USAGE_NODE_MEMORY_SIZE));
+			nodeDTO.setTotalDISK(getValueFromMap(totalDiskMap, nodeName, Promql.NODE_ROOT_DISK_SIZE));
+			nodeDTO.setRequestDISK(getValueFromMap(usageDiskMap, nodeName, Promql.NODE_ROOT_DISK_USAGE_SIZE));
+		}
+
+		return nodeList;
+	}
+	private String getValueFromMap(Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> metricMap, String nodeName, Promql promql) {
+		com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO realTimeDTO = metricMap.get(nodeName);
+		if (realTimeDTO == null) {
+			return "0";
+		}
+		String value = realTimeDTO.value();
+		switch (promql) {
+			case TOTAL_NODE_CPU_CORE:
+			case USAGE_NODE_CPU_CORE:
+				return DataConverterUtil.convertToCPU(value) + " CORE";
+			case TOTAL_NODE_GPU_COUNT:
+			case USAGE_NODE_GPU_COUNT:
+				return DataConverterUtil.convertToGPU(value) + " 개";
+			case TOTAL_NODE_MEMORY_SIZE:
+			case USAGE_NODE_MEMORY_SIZE:
+				return DataConverterUtil.convertToMemorySize(value);
+			case NODE_ROOT_DISK_SIZE:
+			case NODE_ROOT_DISK_USAGE_SIZE:
+				return DataConverterUtil.formatDiskSize(value);
+			default:
+				return "0";
+		}
+	}
+	// MetricType에 따라 요청을 처리하는 메서드 추가
+	private Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> getMetricMap(
+		RequestDTO requestDTO, Promql promql) {
+		requestDTO.changeMetricName(promql.name());
+		List<com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> metricList = prometheusService.getRealTimeMetric(requestDTO);
+		return listToMap(metricList);
+	}
+	private Map<String, com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> listToMap(
+		List<com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO> list) {
+		return list.stream()
+			.collect(Collectors.toMap(com.xiilab.modulemonitor.dto.ResponseDTO.RealTimeDTO::nodeName, dto -> dto));
 	}
 
 	/**
@@ -25,9 +98,10 @@ public class NodeFacadeService {
 	 * @param nodeName 노드의 Name
 	 * @return
 	 */
-	public ResponseDTO.MIGProfile getNodeMIGProfiles(String nodeName){
+	public ResponseDTO.MIGProfile getNodeMIGProfiles(String nodeName) {
 		return nodeRepository.getNodeMIGProfiles(nodeName);
 	}
+
 	/**
 	 * mig profile을 update 하는 메소드
 	 *
