cec10b2dce98634fa82c62de80a7e07efe99facc
hc.park <hc.park@xiilab.com>
2024-03-05 14:24:45 +0900

    [FIX] Bean 이름 중복으로 인한 변경 진행

--- module-alert/src/main/java/com/xiilab/modulealert/enumeration/AlertMessage.java
+++ module-alert/src/main/java/com/xiilab/modulealert/enumeration/AlertMessage.java
@@ -12,6 +12,8 @@ public enum AlertMessage {
 	WORKSPACE_ERROR("워크스페이스(%s) 에러 발생했습니다."),
 	// 리소스
 	RESOURCE_APPROVAL("리소스 승인되었습니다."),
+	RESOURCE_OPTIMIZATION_ALERT("워크스페이스(%s)에 속한 워크로드(%s)가 관리자가 지정한 자원정책에 따라 사용량이 낮아 삭제 될 예정입니다."),
+	RESOURCE_OPTIMIZATION_RESULT("워크스페이스(%s)에 속한 워크로드(%s)가 관리자가 지정한 자원정책에 따라 삭제 되었습니다.")
 	;
 	private final String message;
 }
