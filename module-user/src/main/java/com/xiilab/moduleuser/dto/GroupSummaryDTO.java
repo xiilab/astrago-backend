70bae48d890d9671a9b9522005c0a2ef3dcaed0d
leeyongchun <dldydcns123@gmail.com>
2024-03-07 13:55:39 +0900

    fix : 그룹 별 유저 수 추가

--- module-user/src/main/java/com/xiilab/moduleuser/dto/GroupSummaryDTO.java
+++ module-user/src/main/java/com/xiilab/moduleuser/dto/GroupSummaryDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
+import org.keycloak.admin.client.resource.GroupResource;
 import org.keycloak.representations.idm.GroupRepresentation;
 
 import lombok.Getter;
@@ -15,11 +16,27 @@ public class GroupSummaryDTO {
 	private LocalDate createdDate;
 	private String createdBy;
 	private String description;
+	private long userCount;
 
 	public GroupSummaryDTO(GroupRepresentation groupRep) {
 		Map<String, List<String>> attributes = groupRep.getAttributes();
 		this.id = groupRep.getId();
 		this.name = groupRep.getName();
+		if (attributes != null) {
+			this.createdDate =
+				attributes.get("createdDate") != null ? LocalDate.parse(attributes.get("createdDate").get(0)) : null;
+			this.createdBy =
+				attributes.get("createdBy") != null ? attributes.get("createdBy").get(0) : null;
+			this.description =
+				attributes.get("description") != null ? attributes.get("description").get(0) : null;
+		}
+	}
+	public GroupSummaryDTO(GroupResource group) {
+		GroupRepresentation groupRepresentation = group.toRepresentation();
+		Map<String, List<String>> attributes = groupRepresentation.getAttributes();
+		this.id = groupRepresentation.getId();
+		this.name = groupRepresentation.getName();
+		this.userCount = group.members().size();
 		if (attributes != null) {
 			this.createdDate =
 				attributes.get("createdDate") != null ? LocalDate.parse(attributes.get("createdDate").get(0)) : null;
