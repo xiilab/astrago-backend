75bec27a94dfd83ad20fa2cbbd0a3d88407493cb
junjuno5 <j.seo@xiilab.com>
2024-03-08 10:36:14 +0900

    [fix] 허브 DTO 수정

--- server-core/src/main/java/com/xiilab/servercore/hub/controller/HubController.java
+++ server-core/src/main/java/com/xiilab/servercore/hub/controller/HubController.java
@@ -15,6 +15,7 @@ import com.xiilab.servercore.hub.dto.HubReqDTO;
 import com.xiilab.servercore.hub.dto.response.FindHubResDTO;
 import com.xiilab.servercore.hub.service.HubService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,17 +28,20 @@ public class HubController {
 	private final HubService hubService;
 
 	@GetMapping()
+	@Operation(summary = "Hub 목록 조회")
 	public ResponseEntity<FindHubResDTO.Hubs> getHubList(@RequestParam(required = false) String[] categoryNames,
 		Pageable pageable) {
 		return new ResponseEntity<>(hubService.getHubList(categoryNames, pageable), HttpStatus.OK);
 	}
 
 	@GetMapping("/{hubId}")
+	@Operation(summary = "Hub 상세 조회")
 	public ResponseEntity<FindHubResDTO.HubDetail> getHubResDtoByHubId(@PathVariable("hubId") Long hubId) {
 		return new ResponseEntity<>(hubService.getHubByHubId(hubId), HttpStatus.OK);
 	}
 
 	@PostMapping()
+	@Operation(summary = "Hub 저장")
 	public ResponseEntity<Void> saveHub(@RequestBody HubReqDTO.SaveHub saveHubDTO) {
 		hubService.saveHub(saveHubDTO);
 		return new ResponseEntity<>(HttpStatus.OK);
