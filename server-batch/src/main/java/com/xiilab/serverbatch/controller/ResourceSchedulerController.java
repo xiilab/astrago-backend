d9c098d02373455f82da5aa6836d6bab6a282a07
hc.park <hc.park@xiilab.com>
2024-03-18 16:00:53 +0900

    [FIX] 리소스최적화 API 수정

--- server-batch/src/main/java/com/xiilab/serverbatch/controller/ResourceSchedulerController.java
+++ server-batch/src/main/java/com/xiilab/serverbatch/controller/ResourceSchedulerController.java
@@ -3,11 +3,9 @@ package com.xiilab.serverbatch.controller;
 import org.quartz.SchedulerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.xiilab.serverbatch.common.BatchJob;
 import com.xiilab.serverbatch.dto.ResourceOptimizationDTO;
 import com.xiilab.serverbatch.dto.ResourceOptimizerStatus;
-import com.xiilab.serverbatch.schedulerService.ResourceSchedulerService;
+import com.xiilab.serverbatch.service.ResourceSchedulerService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,31 +25,15 @@ public class ResourceSchedulerController {
 
 	@GetMapping()
 	public ResponseEntity<ResourceOptimizerStatus> getResourceOptimizationSchedulerStatus() throws SchedulerException {
-		return new ResponseEntity<>(resourceSchedulerService.getResourceOptimizerStatus(), HttpStatus.OK);
-	}
-
-	@PostMapping("/{jobType}")
-	public ResponseEntity<HttpStatus> registerResourceOptimizationScheduler(
-		@PathVariable("jobType") BatchJob batchJob,
-		@RequestBody ResourceOptimizationDTO resourceOptimizationDTO) throws Exception {
-		resourceSchedulerService.registerResourceScheduler(resourceOptimizationDTO, batchJob);
-		return ResponseEntity.ok().build();
+		return new ResponseEntity<>(resourceSchedulerService.getResourceSchedulerStatus(), HttpStatus.OK);
 	}
 
 	@PatchMapping("/{jobType}")
 	public ResponseEntity<HttpStatus> updateResourceOptimizationValue(
 		@PathVariable("jobType") BatchJob batchJob,
-		@RequestBody ResourceOptimizationDTO resourceOptimizationDTO
-	) throws Exception {
+		@RequestBody ResourceOptimizationDTO resourceOptimizationDTO) throws Exception {
 		resourceSchedulerService.updateResourceOptimizationScheduler(resourceOptimizationDTO, batchJob);
 		return ResponseEntity.ok().build();
 	}
-
-	@DeleteMapping("{jobType}")
-	public ResponseEntity<HttpStatus> stopResourceOptimizationScheduler(
-		@PathVariable("jobType") BatchJob batchJob
-	) throws SchedulerException {
-		resourceSchedulerService.stopResourceScheduler(batchJob);
-		return ResponseEntity.ok().build();
-	}
 }
+
