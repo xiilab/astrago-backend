d70ea10a892e8344d64de0a5b154c9997193559a
junjuno5 <j.seo@xiilab.com>
2024-03-19 12:28:07 +0900

    [fix] 워크로드 관리자 Alert event 추가

--- module-k8s-db/src/main/java/com/xiilab/modulek8sdb/alert/systemalert/repository/SystemAlertRepositoryCustom.java
+++ module-k8s-db/src/main/java/com/xiilab/modulek8sdb/alert/systemalert/repository/SystemAlertRepositoryCustom.java
@@ -3,10 +3,11 @@ package com.xiilab.modulek8sdb.alert.systemalert.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.xiilab.modulecommon.enums.ReadYN;
 import com.xiilab.modulek8sdb.alert.systemalert.entity.SystemAlertEntity;
 import com.xiilab.modulek8sdb.alert.systemalert.enumeration.SystemAlertType;
 import com.xiilab.modulek8sdb.hub.entity.HubEntity;
 
 public interface SystemAlertRepositoryCustom {
-	Page<SystemAlertEntity> findAlerts(String recipientId, SystemAlertType systemAlertType, Pageable pageable);
+	Page<SystemAlertEntity> findAlerts(String recipientId, SystemAlertType systemAlertType, ReadYN readYN, Pageable pageable);
 }
