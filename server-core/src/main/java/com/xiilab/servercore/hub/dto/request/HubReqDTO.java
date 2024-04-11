b0971eaa80f94c7a8e26621629507c25ea8bb7fd
hc.park <hc.park@xiilab.com>
2024-04-11 17:15:37 +0900

    [FIX] HUB hyperParameter -> parameter로 변경

--- server-core/src/main/java/com/xiilab/servercore/hub/dto/request/HubReqDTO.java
+++ server-core/src/main/java/com/xiilab/servercore/hub/dto/request/HubReqDTO.java
@@ -1,15 +1,12 @@
 package com.xiilab.servercore.hub.dto.request;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.xiilab.modulecommon.enums.WorkloadType;
 import com.xiilab.modulek8sdb.hub.enums.HubLabelType;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -31,19 +28,7 @@ public class HubReqDTO {
 		private String datasetMountPath;
 		private Map<String, String> envMap;
 		private String command;
-		private List<HyperParam> hyperParams;
+		private Map<String,String> parameter;
 		private WorkloadType workloadType;
-
-		public Map<String, String> convertHypterParmToMap() {
-			return hyperParams.stream().collect(Collectors.toMap(HyperParam::getKey, HyperParam::getDescription));
-		}
-	}
-
-	@Getter
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class HyperParam {
-		private String key;
-		private String description;
 	}
 }
