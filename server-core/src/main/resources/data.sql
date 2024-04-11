2e60b298589026e95555f714c8b95b53049c5325
junjuno5 <j.seo@xiilab.com>
2024-04-11 18:09:24 +0900

    [fix] 워크로드 등록, 상세정보 조회 - hub 환경변수

--- server-core/src/main/resources/data.sql
+++ server-core/src/main/resources/data.sql
@@ -787,8 +787,7 @@ create table if not exists TB_WORKSPACE_ALERT_SETTING
     WORKLOAD_END_ALERT      bit          null,
     WORKLOAD_ERROR_ALERT    bit          null,
     WORKLOAD_START_ALERT    bit          null,
-    ID                      bigint auto_increment
-        primary key,
+    ID                      bigint auto_increment primary key,
     WORKSPACE_NAME          varchar(255) null
 );
 
@@ -797,8 +796,7 @@ create table if not exists TB_WORKSPACE_SETTING
     cpu double not null,
     gpu int    not null,
     mem double not null,
-    id  bigint auto_increment
-        primary key
+    id  bigint auto_increment primary key
 );
 
 
