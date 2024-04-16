c16f517e3e5a85f518358ee2302a95780ca79f7b
hc.park <hc.park@xiilab.com>
2024-04-16 11:05:25 +0900

    [FIX] @Param 추가

--- module-k8s-db/src/main/java/com/xiilab/modulek8sdb/image/repository/ImageWorkloadMappingRepository.java
+++ module-k8s-db/src/main/java/com/xiilab/modulek8sdb/image/repository/ImageWorkloadMappingRepository.java
@@ -3,6 +3,7 @@ package com.xiilab.modulek8sdb.image.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.xiilab.modulek8sdb.image.entity.ImageWorkloadMappingEntity;
 
@@ -11,5 +12,5 @@ public interface ImageWorkloadMappingRepository extends JpaRepository<ImageWorkl
 	@Query("update ImageWorkloadMappingEntity dwme "
 		+ "set dwme.deleteYN = 'Y'"
 		+ "where dwme.workload.id =:jobId")
-	void deleteByWorkloadId(Long jobId);
+	void deleteByWorkloadId(@Param("jobId") Long jobId);
 }
