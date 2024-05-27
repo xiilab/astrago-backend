fd36321b306fdb9207d9133216c09b6c28661b4a
junjuno5 <j.seo@xiilab.com>
2024-05-27 15:29:29 +0900

    [fix] TUS 만료 파일 삭제 스케줄러 추가

--- server-batch/src/main/java/com/xiilab/serverbatch/config/FrameworkVersionQuartzConfig.java
+++ server-batch/src/main/java/com/xiilab/serverbatch/config/FrameworkVersionQuartzConfig.java
@@ -1,15 +1,10 @@
 package com.xiilab.serverbatch.config;
 
-import org.quartz.CronScheduleBuilder;
-import org.quartz.CronTrigger;
-import org.quartz.Job;
-import org.quartz.JobBuilder;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
 import org.springframework.context.annotation.Configuration;
 
 import com.xiilab.serverbatch.job.FrameworkVersionJob;
@@ -19,39 +14,18 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class FrameworkVersionQuartzConfig {
+public class FrameworkVersionQuartzConfig extends CommonQuartzConfig {
 	private final Scheduler scheduler;
 
 	@PostConstruct
-	private void jobProcess() throws SchedulerException {
+	protected void jobProcess() throws SchedulerException {
 		cronScheduler();
 	}
 
-	private JobDetail createJobDetail(Class<? extends Job> jobClass, String jobName, JobDataMap jobDataMap) {
-		JobDetail jobDetail = JobBuilder.newJob(jobClass)
-			.withIdentity(jobName)
-			.setJobData(jobDataMap)
-			.storeDurably()
-			.build();
-		return jobDetail;
-	}
-
-	private Trigger createCronTrigger(JobDetail jobDetail, String triggerName, String cronExpression) {
-		CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(cronExpression);
-		CronTrigger trigger = TriggerBuilder.newTrigger()
-			.forJob(jobDetail)
-			.withIdentity(triggerName)
-			.withSchedule(cronScheduleBuilder)
-			.build();
-		return trigger;
-	}
 	private void cronScheduler() throws SchedulerException {
 		JobDataMap jobDataMap = new JobDataMap();
 		JobDetail frameworkVersionJobDetail = createJobDetail(FrameworkVersionJob.class, "FrameworkVersionJob", jobDataMap);
-
-		// Trigger frameworkVersionJobTrigger = createCronTrigger(frameworkVersionJobDetail, "frameworkVersionJobTrigger", "0/30 * * * * ?");
 		Trigger frameworkVersionJobTrigger = createCronTrigger(frameworkVersionJobDetail, "frameworkVersionJobTrigger", "0 0 2 * * ?"); //매일 새벽 2시
 		scheduler.scheduleJob(frameworkVersionJobDetail, frameworkVersionJobTrigger);
 	}
-
 }
