fbb5097c046b7d57499223ad97a8f9981d0e2e8f
KimYeonHun <y.kim@xiilab.com>
2024-06-10 16:47:09 +0900

    [FEAT] SMTP 등록 Websecurity 제외

--- server-core/src/main/java/com/xiilab/servercore/security/WebSecurityConfig.java
+++ server-core/src/main/java/com/xiilab/servercore/security/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -41,6 +40,7 @@ public class WebSecurityConfig {
 			.requestMatchers("/api/v1/core/license/**").permitAll()
 			.requestMatchers(HttpMethod.GET, "/swagger-ui/**", "/v3/api-docs/**").permitAll()
 			.requestMatchers(HttpMethod.POST, "/api/v1/core/alertManager/receive").permitAll()
+			.requestMatchers(HttpMethod.POST, "/api/v1/core/smtp").permitAll()
 			.requestMatchers("/ws/**").permitAll()
 			.requestMatchers("/api/v1/core/tus/**").permitAll()
 			.anyRequest().authenticated());
