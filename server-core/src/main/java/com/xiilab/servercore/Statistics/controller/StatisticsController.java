34145c6d75036b4a2b188f6f2ff208292f6aaa60
KimYeonHun <y.kim@xiilab.com>
2024-06-11 15:04:11 +0900

    [FEAT] user statistics

---
+++ server-core/src/main/java/com/xiilab/servercore/Statistics/controller/StatisticsController.java
@@ -0,0 +1,36 @@
+package com.xiilab.servercore.Statistics.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xiilab.modulek8sdb.statistics.repository.StatisticsRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/core/statistics")
+@RequiredArgsConstructor
+public class StatisticsController {
+	private final StatisticsRepository statisticsRepository;
+
+	@GetMapping("/test")
+	public ResponseEntity<HttpStatus> test() {
+
+		statisticsRepository.resourceUsageDTOList();
+		statisticsRepository.resourceRequestDTOList();
+		statisticsRepository.getUserResourceRequestCount();
+		statisticsRepository.resourceQuotaApproveCount();
+		statisticsRepository.resourceQuotaRejectCount();
+		statisticsRepository.getCreateWorkloadCount();
+		statisticsRepository.getCreateCodeCount();
+		statisticsRepository.getCreateDatasetCount();
+		statisticsRepository.getCreateCredentialCount();
+
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+}
