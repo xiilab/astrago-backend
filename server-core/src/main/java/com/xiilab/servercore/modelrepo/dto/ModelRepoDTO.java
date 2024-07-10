39db71378620cad7231fab60390ee5773389ab3c
KimYeonHun <y.kim@xiilab.com>
2024-07-10 15:26:14 +0900

    [FEAT] model repo DTO 생성

--- server-core/src/main/java/com/xiilab/servercore/modelrepo/dto/ModelRepoDTO.java
+++ server-core/src/main/java/com/xiilab/servercore/modelrepo/dto/ModelRepoDTO.java
@@ -2,7 +2,11 @@ package com.xiilab.servercore.modelrepo.dto;
 
 import java.util.List;
 
+import com.xiilab.modulecommon.enums.StorageType;
 import com.xiilab.modulek8sdb.modelrepo.entity.ModelRepoEntity;
+import com.xiilab.modulek8sdb.modelrepo.entity.ModelVersionEntity;
+import com.xiilab.modulek8sdb.storage.entity.StorageEntity;
+import com.xiilab.servercore.label.dto.LabelDTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,21 +20,24 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class ModelRepoDTO {
 
-	protected List<LabelDTO> labels;
-	protected String storagePath;
 	protected String modelName;
 	protected String description;
+	protected String workspaceResourceName;
+	protected List<LabelDTO> labels;
 
 	@Getter
 	@AllArgsConstructor
 	@NoArgsConstructor
 	@SuperBuilder
 	public static class RequestDTO extends ModelRepoDTO {
-		public ModelRepoEntity convertEntity() {
+		protected long storageId;
+
+		public ModelRepoEntity convertEntity(StorageEntity storageEntity) {
 			return ModelRepoEntity.builder()
-				.storagePath(this.getStoragePath())
 				.description(this.getDescription())
 				.modelName(this.getModelName())
+				.workspaceResourceName(this.getWorkspaceResourceName())
+				.storageEntity(storageEntity)
 				.build();
 		}
 	}
@@ -40,18 +47,43 @@ public class ModelRepoDTO {
 	@NoArgsConstructor
 	@SuperBuilder
 	public static class ResponseDTO extends ModelRepoDTO {
-		private String version;
-
+		private long modelRepoId;
+		private List<VersionDTO> version;
+		private StorageType storageType;
+		private String storageName;
+		private String ip;
+		private String storagePath;
+		public static ModelRepoDTO.ResponseDTO convertModelRepoDTO(ModelRepoEntity modelRepoEntity) {
+			return ResponseDTO.builder()
+				.modelRepoId(modelRepoEntity.getId())
+				.workspaceResourceName(modelRepoEntity.getWorkspaceResourceName())
+				.modelName(modelRepoEntity.getModelName())
+				.description(modelRepoEntity.getDescription())
+				.storageName(modelRepoEntity.getStorageEntity().getStorageName())
+				.storageType(modelRepoEntity.getStorageEntity().getStorageType())
+				.storagePath(modelRepoEntity.getStorageEntity().getStoragePath())
+				.ip(modelRepoEntity.getStorageEntity().getIp())
+				.labels(modelRepoEntity.getModelLabelEntityList().stream().map(modelLabelEntity -> LabelDTO.convertLabelDTO(modelLabelEntity.getLabelEntity())).toList())
+				.version(modelRepoEntity.getModelVersionList().stream().map(VersionDTO::convertVersionDTO).toList())
+				.build();
+		}
 	}
 
 	@Getter
 	@Builder
 	@AllArgsConstructor
 	@NoArgsConstructor
-	public static class LabelDTO {
-
+	public static class VersionDTO {
+		private long versionId;
 		// 라벨은 model에 종속됨
-		private String name;
+		private String versionName;
+
+		public static VersionDTO convertVersionDTO(ModelVersionEntity versionEntity) {
+			return VersionDTO.builder()
+				.versionId(versionEntity.getId())
+				.versionName(versionEntity.getVersion())
+				.build();
+		}
 	}
 
 }
