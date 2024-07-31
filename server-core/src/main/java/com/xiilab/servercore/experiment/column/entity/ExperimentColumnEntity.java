41e25ad0b5c5302af3ac8ea9c719365abe9c7ee0
hc.park <hc.park@xiilab.com>
2024-07-31 09:26:28 +0900

    [FEAT] experiment column 기능 개발

---
+++ server-core/src/main/java/com/xiilab/servercore/experiment/column/entity/ExperimentColumnEntity.java
@@ -0,0 +1,41 @@
+package com.xiilab.servercore.experiment.column.entity;
+
+import com.xiilab.modulek8sdb.common.entity.BaseEntity;
+import com.xiilab.servercore.experiment.column.dto.ExperimentColumnDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TB_EXPERIMENT_COLUMN")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExperimentColumnEntity extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	@Column(name = "NAME")
+	private String name;
+	@Column(name = "ORDER")
+	private int order;
+	@Column(name = "workspace")
+	private String workspace;
+
+	public ExperimentColumnEntity(ExperimentColumnDTO.Req req) {
+		this.name = req.getName();
+		this.order = req.getOrder();
+		this.workspace = req.getWorkspace();
+	}
+
+	public void updateOrder(int order) {
+		this.order = order;
+	}
+}
