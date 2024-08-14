856132a336a80cc916a2d77924d33166d20e131b
junjuno5 <j.seo@xiilab.com>
2024-08-14 19:00:28 +0900

    [fix] 이미지 정보 조회 오류 수정

--- module-k8s-db/src/main/java/com/xiilab/modulek8sdb/workload/history/entity/PortEntity.java
+++ module-k8s-db/src/main/java/com/xiilab/modulek8sdb/workload/history/entity/PortEntity.java
@@ -2,10 +2,9 @@ package com.xiilab.modulek8sdb.workload.history.entity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.util.CollectionUtils;
-
-import com.xiilab.modulek8sdb.workload.history.dto.PortDTO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,10 +12,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.util.Collections;
-import java.util.Map;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,6 +37,7 @@ public class PortEntity {
 	@Column(name = "PORT_TARTGET_NUM")
 	private Integer targetPortNum;
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "WORKLOAD_ID")
 	private WorkloadEntity workload;
 
 	public void setWorkload(WorkloadEntity workload){
