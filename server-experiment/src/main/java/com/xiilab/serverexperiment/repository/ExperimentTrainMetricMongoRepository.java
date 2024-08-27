903d09fb58ad7e0255240f81843ff82826271f52
hc.park <hc.park@xiilab.com>
2024-08-27 10:09:00 +0900

    [refactor] experiment -> experimentTrainMetric로 변경

--- server-experiment/src/main/java/com/xiilab/serverexperiment/repository/ExperimentTrainMetricMongoRepository.java
+++ server-experiment/src/main/java/com/xiilab/serverexperiment/repository/ExperimentTrainMetricMongoRepository.java
@@ -3,8 +3,8 @@ package com.xiilab.serverexperiment.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.xiilab.serverexperiment.domain.mongo.Experiment;
+import com.xiilab.serverexperiment.domain.mongo.ExperimentTrainMetric;
 
 @Repository
-public interface ExperimentMongoRepository extends MongoRepository<Experiment, String> {
+public interface ExperimentTrainMetricMongoRepository extends MongoRepository<ExperimentTrainMetric, String> {
 }
