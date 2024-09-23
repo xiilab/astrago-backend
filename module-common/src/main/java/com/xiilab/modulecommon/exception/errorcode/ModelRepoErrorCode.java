8fd40fdbe3158ed58b5c20abf619a8271bffeb44
leeyoungchun-xiilab <yc.lee@xiilab.com>
2024-09-23 17:01:06 +0900

    fix: 사용중인 모델은 삭제안되게 수정

--- module-common/src/main/java/com/xiilab/modulecommon/exception/errorcode/ModelRepoErrorCode.java
+++ module-common/src/main/java/com/xiilab/modulecommon/exception/errorcode/ModelRepoErrorCode.java
@@ -13,6 +13,7 @@ public enum ModelRepoErrorCode implements ErrorCode {
 	MODEL_REPO_SAVE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "모델 등록에 실패하였습니다."),
 	MODEL_REPO_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR.value(), "해당 ID의 모델이 없습니다."),
 	MODEL_REPO_VERSION_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR.value(), "해당 ID의 모델 버전이 없습니다."),
+	MODEL_REPO_DELETE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "해당 모델을 사용중인 서비스가 있습니다. 사용중인 모델은 삭제할 수 없습니다."),
 
 	;
 	private final int code;
