968e8b63f13ff49aca686a3b2600c56eac7338ec
junjuno5 <j.seo@xiilab.com>
2024-10-30 17:49:36 +0900

    [fix] 첨부파일 여러개 업로드 오류 수정

--- module-k8s-db/src/main/java/com/xiilab/modulek8sdb/board/repository/BoardRepositoryCustom.java
+++ module-k8s-db/src/main/java/com/xiilab/modulek8sdb/board/repository/BoardRepositoryCustom.java
@@ -1,15 +1,12 @@
 package com.xiilab.modulek8sdb.board.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.xiilab.modulecommon.enums.SortType;
-import com.xiilab.modulek8sdb.board.entity.BoardAttachedFileEntity;
 import com.xiilab.modulek8sdb.board.entity.BoardEntity;
 
 public interface BoardRepositoryCustom {
 	Page<BoardEntity> findBoards(SortType sortType, String searchText, Pageable pageable);
-	void saveAll(List<BoardAttachedFileEntity> boardAttachedFileEntityList);
+	// void saveAll(List<BoardAttachedFileEntity> boardAttachedFileEntityList);
 }
